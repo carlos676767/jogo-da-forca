def5fcdc3848f639a3f61b60c4bcfc8e15e0f1b0
carlos676767 <carlitobr730@gmail.com>
2024-01-31 10:24:53 -0800

    projeto jogo da forca

---
+++ jogo/src/main.ts
@@ -0,0 +1,28 @@
+
+type Palavras = {
+  palavra1: string;
+  palavra2: string;
+  palavra3: string;
+  [key: string]: string;
+};
+
+const meuObjeto: Palavras = {
+  palavra1: "banana",
+  palavra2: "uva",
+  palavra3: "abacate",
+};
+
+const array: string[] = []
+const percorrerEsorteia = () => {
+  const chaves = Object.keys(meuObjeto);
+  const sorteio = chaves[Math.floor(Math.random() * chaves.length)];
+  const obter = meuObjeto[sorteio];
+  array.push(obter)
+};
+percorrerEsorteia()
+
+const botao = document.querySelector("button")
+const p = document.querySelector("p")
+
+
+
